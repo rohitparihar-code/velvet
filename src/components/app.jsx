ac24a160b7ee57859d1b5f7dd93fb8d1cf7dd80c
rohitparihar-code <rohitparihar2002@gmail.com>
2021-06-26 23:44:10 +0530

    view-all-blog file added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
+import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    // Link,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link,
 } from "react-router-dom";
 
-import Login from './login';
-import SignUp from './signup';
-import Quest from './questionnaire';
-import ChatRoomNames from './chatRoomNames';
-import ChatRoom from './chat_room';
-import ViewAllBlogs from './view_all_blogs';
+import Login from "./login";
+import SignUp from "./signup";
+import Quest from "./questionnaire";
+import ChatRoomNames from "./chatRoomNames";
+import ChatRoom from "./chat_room";
+import ViewAllBlogs from "./view_all_blogs";
 
 function App() {
-    return (
-        <Router basename='/'>
-            <div>
-                {/* A <Switch> looks through its children <Route>s and
+  return (
+    <Router basename="/">
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-                <Switch>
-                    <Route path="/signup" component={SignUp}></Route>
-                    <Route path="/quest" component={Quest}></Route>
-                    <Route path="/chat-room-names" component={ChatRoomNames}></Route>
-                    <Route path="/chat-room" component={ChatRoom}></Route>
-                    <Route path="/view-all-blogs" component={ViewAllBlogs}></Route>
-                    <Route path="/" component={Login}></Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+        <Switch>
+          <Route path="/signup" component={SignUp}></Route>
+          <Route path="/quest" component={Quest}></Route>
+          <Route path="/chat-room-names" component={ChatRoomNames}></Route>
+          <Route path="/chat-room" component={ChatRoom}></Route>
+          <Route path="/view-all-blogs" component={ViewAllBlogs}></Route>
+          <Route path="/" component={Login}></Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App; 
+export default App;
--- src/components/view_all_blogs.jsx
+++ src/components/view_all_blogs.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
 export default class ViewAllBlogs extends React.Component {
-    constructor(props) {
-        super(props);
-        this.idkwhat = '';
-    }
+  constructor(props) {
+    super(props);
+    this.idkwhat = "";
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>Hello World</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="container-fluid">
+          <h1>Heading</h1>
+        </div>
+      </div>
+    );
+  }
 }
