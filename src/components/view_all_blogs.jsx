10e0d31fe180387ce2ba03fb3fee377f0e2bab7b
rohitparihar-code <rohitparihar2002@gmail.com>
2021-06-27 11:13:25 +0530

    Blog design

--- src/components/view_all_blogs.jsx
+++ src/components/view_all_blogs.jsx
@@ -10,15 +10,15 @@ export default class ViewAllBlogs extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="my-body">
         <div className="container-fluid view-blogs-landing-div row">
-          <div className="col-5 align-self-center my-box">
-            <h3>
-              <a href="#blogs">Read Stories From <br /> Inspiring People </a>
+          <div className="col-5 justify-content-center my-box">
+            <h3 className="align-self-center" Style="padding-top: 14vh">
+              <a className="my-link" href="#blogs">Read Stories From <br /> Inspiring People </a>
             </h3>
           </div>
           <div className="col-5 align-self-center my-box">
-            <h3> Write Your Own Story </h3>
+            <h3 className="align-self-center" Style="padding-top: 14vh"> Write Your Own Story </h3>
             <button
               className="btn btn-outline-warning"
               data-bs-toggle="modal"
@@ -31,7 +31,77 @@ export default class ViewAllBlogs extends React.Component {
           <h2>Read Stories from Peoples who have overcome fear of hiding</h2>
         </div>
         <div id="blogs" className="blog-content m-5">
-          <Card style={{ width: "60vw", height: "37vh" }}>
+          <Card style={{ width: "60vw", height: "37vh", margin: "50px" }}>
+            <Card.Body>
+              <div className="row justify-content-between">
+                <div className="col-9">
+                  <Card.Text>Author</Card.Text>
+                  <Card.Title style={{ height: "4vh", overflow: "hidden" }}>
+                    Some very large Blog title
+                  </Card.Title>
+                  <Card.Text style={{ height: "10vh", overflow: "hidden" }}>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has
+                  </Card.Text>
+                  <button
+                    className="btn"
+                    data-bs-toggle="modal"
+                    data-bs-target="#readArticleModal"
+                  >
+                    Read Full Story
+                  </button>
+                </div>
+                <div className="col-3">
+                  <Card.Img
+                    style={{ height: "30vh", width: "13vw", objectFit: "fill" }}
+                    variant="top"
+                    src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80"
+                  />
+                  <hr />
+                </div>
+              </div>
+            </Card.Body>
+            <hr />
+          </Card>
+          <Card style={{ width: "60vw", height: "37vh", margin: "50px" }}>
+            <Card.Body>
+              <div className="row justify-content-between">
+                <div className="col-9">
+                  <Card.Text>Author</Card.Text>
+                  <Card.Title style={{ height: "4vh", overflow: "hidden" }}>
+                    Some very large Blog title
+                  </Card.Title>
+                  <Card.Text style={{ height: "10vh", overflow: "hidden" }}>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has
+                  </Card.Text>
+                  <button
+                    className="btn"
+                    data-bs-toggle="modal"
+                    data-bs-target="#readArticleModal"
+                  >
+                    Read Full Story
+                  </button>
+                </div>
+                <div className="col-3">
+                  <Card.Img
+                    style={{ height: "30vh", width: "13vw", objectFit: "fill" }}
+                    variant="top"
+                    src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80"
+                  />
+                  <hr />
+                </div>
+              </div>
+            </Card.Body>
+            <hr />
+          </Card>
+          <Card style={{ width: "60vw", height: "37vh", margin: "50px" }}>
             <Card.Body>
               <div className="row justify-content-between">
                 <div className="col-9">
